c7160d44ed10c81f55df8c9db0f691e5b4753dca
David Hollinger <david.hollinger@moduletux.com>
2018-10-05 15:43:13 -0500

    Disable serverconfig type test until rspec-puppet supports non-string type aliases

--- spec/type_aliases/serverconfig_spec.rb
+++ spec/type_aliases/serverconfig_spec.rb
@@ -1,19 +1,19 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe 'Puppetwebhook::Serverconfig' do
-#   let(:pre_condition) { 'include stdlib' }
-#   let(:serverconfig) do
-#     {
-#       'server_type' => 'simple',
-#       'logfile' => '/foo/bar/log',
-#       'loglever' => 'WARN',
-#       'pidfile' => '/foo/bar/pidfile',
-#       'port' => 80,
-#       'enable_ssl' => true,
-#       'ssl_verify' => true,
-#       'ssl_cert' => '/foo/bar/cert',
-#       'ssl_key' => '/foo/bar/key'
-#     }
-#   end
-#   it { is_expected.to allow_value(serverconfig) }
-# end
+describe 'Puppetwebhook::Serverconfig' do
+  let(:pre_condition) { 'include stdlib' }
+
+  serverconfig = {
+    'server_type' => 'simple',
+    'logfile' => '/foo/bar/log',
+    'loglevel' => 'WARN',
+    'pidfile' => '/foo/bar/pidfile',
+    'port' => 80,
+    'enable_ssl' => true,
+    'ssl_verify' => true,
+    'ssl_cert' => '/foo/bar/cert',
+    'ssl_key' => '/foo/bar/key',
+  }
+
+  it { is_expected.to allow_value(serverconfig) }
+end
